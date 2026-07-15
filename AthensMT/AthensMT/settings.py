d6b1ad00c681e748f30c29a28ef4a89d77748d4d
Ashikh Ahamed <40172637+ictashik@users.noreply.github.com>
2026-07-15 11:41:03 +0530

    Update settings.py

--- AthensMT/AthensMT/settings.py
+++ AthensMT/AthensMT/settings.py
@@ -40,7 +40,8 @@ ALLOWED_HOSTS = [
     "datatools.sjri.res.in",
     "localhost",
     "127.0.0.1",
-    '10.10.60.150'
+    '10.10.60.150',
+    '10.60.23.101',
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://datatools.sjri.res.in", 
